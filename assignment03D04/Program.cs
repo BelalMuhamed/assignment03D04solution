af58f5f9903f0cf799d9ca5a3f9398a59340d76b
BelalBasal <basdibrahim@gmail.com>
2024-07-03 12:09:23 +0300

    v02

--- assignment03D04/Program.cs
+++ assignment03D04/Program.cs
@@ -4,6 +4,34 @@
     {
         static void Main(string[] args)
         {
+            //array two dimensional 
+            #region v02
+            int[,] marks = new int[2, 3];
+            bool flag;
+            for (int i = 0; i < marks.GetLength(0); i++)
+            {
+                Console.WriteLine($"enter the student{i+1} marks :");
+                for (int j = 0; j < marks.GetLength(1); )
+                {
+                    Console.WriteLine($"enter mark of subject{j+1}");
+                    flag=int.TryParse(Console.ReadLine(), out marks[i,j]);
+                    if (flag)
+                        j++;
+                }
+            }
+            Console.Clear();
+            Console.Beep(2554, 1000);
+            for (int i = 0; i < marks.GetLength(0); i++)
+            {
+                Console.WriteLine($" the student{i + 1} marks :");
+                for (int j = 0; j < marks.GetLength(1);j++)
+                {
+                    Console.Write($"{marks[i,j]}  ");
+                    
+                }
+                Console.WriteLine();
+            }
+            #endregion
 
         }
     }
