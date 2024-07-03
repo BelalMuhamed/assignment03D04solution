0584ed0b73d82a49337aef291e35da426ddabb5b
BelalBasal <basdibrahim@gmail.com>
2024-07-03 12:48:58 +0300

    v03

--- assignment03D04/Program.cs
+++ assignment03D04/Program.cs
@@ -10,11 +10,11 @@
             bool flag;
             for (int i = 0; i < marks.GetLength(0); i++)
             {
-                Console.WriteLine($"enter the student{i+1} marks :");
-                for (int j = 0; j < marks.GetLength(1); )
+                Console.WriteLine($"enter the student{i + 1} marks :");
+                for (int j = 0; j < marks.GetLength(1);)
                 {
-                    Console.WriteLine($"enter mark of subject{j+1}");
-                    flag=int.TryParse(Console.ReadLine(), out marks[i,j]);
+                    Console.WriteLine($"enter mark of subject{j + 1}");
+                    flag = int.TryParse(Console.ReadLine(), out marks[i, j]);
                     if (flag)
                         j++;
                 }
@@ -24,15 +24,33 @@
             for (int i = 0; i < marks.GetLength(0); i++)
             {
                 Console.WriteLine($" the student{i + 1} marks :");
-                for (int j = 0; j < marks.GetLength(1);j++)
+                for (int j = 0; j < marks.GetLength(1); j++)
                 {
-                    Console.Write($"{marks[i,j]}  ");
-                    
+                    Console.Write($"{marks[i, j]}  ");
+
                 }
                 Console.WriteLine();
             }
             #endregion
+            //two dimensional array judged 
+            #region v03
 
+            int[][] mark = new int[3][];
+            mark[0] = new int[3] {5,8,9};
+            mark[1] = new int[2] { 8, 7 };
+            mark[2] = new int[1] { 1 };
+            int totalElements = 0;
+            for (int i = 0; i < mark.Length; i++)
+            {
+                for(int j = 0; j < mark[i].Length; j++)
+                {
+                    Console.WriteLine(mark[i][j]);
+                }
+            }
+
+            // Calculate the total number of elements
+            
+            #endregion
         }
     }
 }
